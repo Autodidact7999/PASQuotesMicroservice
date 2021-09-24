e3475ca2bf5440a798d2bac2c0ce26641576a2e3
Dell <Dell@DELL-PC>
2021-09-24 12:50:48 +0530

    Add project files.

---
+++ PolicyAdmin.QuotesMS.API/Migrations/20210923191800_First.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PolicyAdmin.QuotesMS.API.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BusinesssValueRangeTo",
+                table: "QuotesMaster",
+                newName: "BusinessValueRangeTo");
+
+            migrationBuilder.RenameColumn(
+                name: "BusinesssValueRangeFrom",
+                table: "QuotesMaster",
+                newName: "BusinessValueRangeFrom");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BusinessValueRangeTo",
+                table: "QuotesMaster",
+                newName: "BusinesssValueRangeTo");
+
+            migrationBuilder.RenameColumn(
+                name: "BusinessValueRangeFrom",
+                table: "QuotesMaster",
+                newName: "BusinesssValueRangeFrom");
+        }
+    }
+}
---
+++ dbo.QuotesMaster.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[QuotesMaster] ON
+INSERT INTO [dbo].[QuotesMaster] ([Id], [PropertyValueRangeFrom], [PropertyValueRangeTo], [BusinessValueRangeFrom], [BusinessValueRangeTo], [PropertyType], [Quote]) VALUES (200, 0, 2, 0, 2, 0, 30300)
+SET IDENTITY_INSERT [dbo].[QuotesMaster] OFF
